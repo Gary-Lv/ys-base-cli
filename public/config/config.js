dfe7b726e4ffa48aab8a03c8b18d132de4c15ee5
GouLanZi <shiquan125521@163.com>
2021-03-02 17:16:28 +0800

    分离配置文件

--- public/config/config.js
+++ public/config/config.js
@@ -1,13 +1,14 @@
-const configInfo = {
-  serviceProxy: {},
-  PaaSCode: "00", // 平台Code码
-  ThemeIndex: 1, // 默认使用的皮肤ID
-  IsBreadcrumb: true, // 是否开启面包屑组件
-  IsHeaderTab: true, // 是否开启头部多Tab切换的组件
-  IsShowCollapsedBtn: true, // 是否显示左侧菜单栏的收起按钮
-  DefUnfold: true, // 菜单默认是否是展开状态
-  IsCaptchaVerify: false, // 是否开启滑块验证功能
-};
+import configInfo from "./config.json";
+// const configInfo = {
+//   serviceProxy: {},
+//   PaaSCode: "00", // 平台Code码
+//   ThemeIndex: 1, // 默认使用的皮肤ID
+//   IsBreadcrumb: true, // 是否开启面包屑组件
+//   IsHeaderTab: true, // 是否开启头部多Tab切换的组件
+//   IsShowCollapsedBtn: true, // 是否显示左侧菜单栏的收起按钮
+//   DefUnfold: true, // 菜单默认是否是展开状态
+//   IsCaptchaVerify: false, // 是否开启滑块验证功能
+// };
 
 //设置属性不可修改
 let NotAlter = (obj) => {
