85d606f1f83daae8caf74c68060ec55f84397c43
GouLanZi <shiquan125521@163.com>
2021-04-15 17:37:02 +0800

    修改配置，简化了项目上每次升级组件库需要传参的问题

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 自适应
+import "ys-lib-flesible";
 
 //基本样式初始化
 import "./assets/normalize.css";
